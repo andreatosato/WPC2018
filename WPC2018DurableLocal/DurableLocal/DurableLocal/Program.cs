77ed2b10d185b53a2451a9291382f93f68ec55b0
Andrea Tosato <andrea.tosato@hotmail.it>
2018-11-18 11:25:09 +0100

    Durable Local

---
+++ WPC2018DurableLocal/DurableLocal/DurableLocal/Program.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Configuration;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace DFWebJobsSample
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var loggerFactory = new LoggerFactory())
+            {
+                var config = new JobHostConfiguration();
+                config.DashboardConnectionString = "UseDevelopmentStorage=true";                
+                config.LoggerFactory = loggerFactory
+                    .AddConsole();
+
+                config.UseTimers();
+                config.UseDurableTask(new DurableTaskExtension
+                {
+                    HubName = "DemoDurableLocal",
+                });
+                var host = new JobHost(config);
+                host.RunAndBlock();
+            }
+        }
+    }
+}
